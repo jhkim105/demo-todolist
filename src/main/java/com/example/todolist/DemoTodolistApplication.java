ba9a0d29f8fa4e698d51dc28a614738cdfdc55ec
jhkim105 <jhkim105@gmail.com>
2019-01-03 18:08:37 +0900

    initial commit - JPA, H2, thymeleaf

---
+++ src/main/java/com/example/todolist/DemoTodolistApplication.java
@@ -0,0 +1,14 @@
+package com.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTodolistApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoTodolistApplication.class, args);
+  }
+
+}
+

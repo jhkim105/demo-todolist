95ac79996a0a29ef33d6c0cf33d81dbb21513adb
jhkim105 <jhkim105@gmail.com>
2020-07-01 13:59:30 +0900

    mysql, mariadb profile

--- src/main/java/com/example/todolist/SwaggerConfig.java
+++ src/main/java/com/example/todolist/SwaggerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.*;
 import java.util.ArrayList;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @ConditionalOnProperty(prefix="swagger", name="enabled", havingValue = "true")
 public class SwaggerConfig {
   @Bean
